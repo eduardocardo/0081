a411cec0308335b83ae567f086445b0cac90886d
eduardo <eduardocardomartinez@gmail.com>
2015-02-26 18:52:29 +0100

    Modificiada la clase Responder para que genere una respuesta determinada si el texto introducido contiene una palabra clave

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -13,6 +14,8 @@ public class Responder
     private Random rnd;
     //arrayList que almacena las respuestas
     private ArrayList<String> respuesta;
+    //mapa que almacena unas respuestas en funcion de una palabra clave
+    private HashMap<String,String> otrasRespuestas;
     
     /**
      * Construct a Responder - nothing to do
@@ -26,14 +29,29 @@ public class Responder
         respuesta.add("No se que decirle");
         respuesta.add("Le paso con otro departamento");
         respuesta.add("Comprese un aparato nuevo");
+        
+        otrasRespuestas = new HashMap<>();
+        otrasRespuestas.put("hola","En que puedo servirle");
+        otrasRespuestas.put("azul","que sale en su pantalla");
+        otrasRespuestas.put("mac","tiene que ponerse en contacto con Apple");
+        otrasRespuestas.put("congelado","pruebe a reiniciar su pc");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String clave)
     {
-        return respuesta.get(rnd.nextInt(5));
+        String contestacion;
+        if(otrasRespuestas.containsKey(clave))    //se comprueba si el parametro introducido corresponde con alguna key del mapa
+        {
+            contestacion = otrasRespuestas.get(clave);       //se almacena en la variable el valor asociado a esa key
+        }
+        else
+        {
+            contestacion = respuesta.get(rnd.nextInt(5));    //se genera una respuesta aleatoria de la lista existente
+        }
+        return contestacion;
     }
 }
